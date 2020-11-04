cfc51fd878c5474e489f154317edf464224b80b3
Your Name <you@example.com>
2020-11-04 19:34:14 +0200

    postilaatikko beta

--- public/js/pankkitiedot.js
+++ public/js/pankkitiedot.js
@@ -5,7 +5,31 @@ function laheta_pankkitiedot() {
 
   console.log(nimi);
   console.log(pin);
+
+  const data = {nimi, pin};
+
+  const options = {
+    method: "POST",
+    headers: {
+        "Content-Type":"aplication/json"
+    },
+    body: JSON.stringify(data)
+  };
+
+  fetch('/pankkitietojentalletus',options).then(function(response) {
+    if(response.status == 200){
+        console.log("ok");
+    }
+  }, function(error){
+    console.log(error.message)
+  });
+
+
+
+
+
 }
+
 
 async function haepankkitiedot(){
   const response = await fetch("/salaisetpankkitiedot");
